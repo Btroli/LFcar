4bbff6b4670b4dea3a0d474bb90c75b99aa14c90
zkt <zkt2333@qq.com>
2025-11-15 23:59:42 +0800

    计算value(不精)

--- C/main.c
+++ C/main.c
@@ -58,6 +58,7 @@ void loop_screen0(void);
 void loop_screen1(void);
 void loop_car(void);
 void FuXuan(uint8_t n);
+float cal_value(uint8_t n);
 
 /********************************************/
 
@@ -195,4 +196,13 @@ void FuXuan(uint8_t n) {
 		OLED_DrawBoxXuLine(0, 8 + 18 * n, 127, 26 + 18 * n, 3, 0);
 		OLED_DrawBoxXuLine(2, 10 + 18 * n, 125, 24 + 18 * n, 3, 0);
 	}
+}
+float cal_value(uint8_t n) {
+	static const float pow10[10] = {
+		1e-9f, 1e-8f, 1e-7f, 1e-6f, 1e-5f, 1e-4f, 1e-3f, 1e-2f, 1e-1f, 1e0f
+	};
+	float m = 0;
+	for (uint8_t i = 0; i < 9; ++i)
+		m = m * 10 + value[n][i];
+	return m * pow10[value[n][9]];
 }
