8940a48c6fe9fa77c9175e23cc6789d53ad454a0
zkt <zkt2333@qq.com>
2025-11-26 15:42:00 +0800

    妙妙工具2

--- C/USER_h/mmgj.h
+++ C/USER_h/mmgj.h
@@ -3,10 +3,20 @@
 
 #include "stdint.h"
 #include "oled.h"
+#include "key.h"
+
+#define value_len 10
+extern uint8_t count, area, option, option_NUM, sel_flag, car_screen_flag, value_num;
+extern uint8_t value[10][value_len] , name[10][10];
 
 void cal_valueP(uint8_t *p, float *v);
 float cal_value(uint8_t *p);
 void FuXuan(uint8_t n);
+
+void loop_screen0(void);
+void loop_screen1(void);
+
+void pidInit(void);
 
 #endif
 
--- C/main.c
+++ C/main.c
@@ -24,8 +24,7 @@
 //
 //int Encoder_A, Encoder_B;
 
-uint8_t count = 1, area = 0, option = 0, option_NUM = 10, sel_flag = 1, car_screen_flag = 0
-                                      , value_num = 0;
+uint8_t count = 1, area = 0, option = 0, option_NUM = 10, sel_flag = 1, car_screen_flag = 0, value_num = 0;
 
 int16_t EvalueA = 0, EvalueB = 0;
 
@@ -66,8 +65,8 @@ uint8_t name[10][10] = {
 
 /********************************************/
 
-void loop_screen0(void);
-void loop_screen1(void);
+//void loop_screen0(void);
+//void loop_screen1(void);
 void loop_car(void);
 void pidInit(void);
 
@@ -148,91 +147,6 @@ void loop_car(void) {
 
 
 	delay_ms(200);
-}
-void loop_screen1(void) {
-	if (KEY_Scan(1)) {
-		value_num++;
-		if (value_num == value_len)
-			value_num = 0;
-	}
-	if (KEY_Scan(2)) {
-		if (value_num == value[option][value_len - 1]) {
-			value[option][value_len - 1] += (value[option][value_len - 1] == 9) ? -9 : 1;
-			value_num += (value_num == value_len - 1) ? 1 - value_len : 1;
-		} else if (value_num < value[option][value_len - 1])
-			value[option][value_num] += (value[option][value_num] == 9) ? -9 : 1;
-		else
-			value[option][value_num - 1] += (value[option][value_num - 1] == 9) ? -9 : 1;
-	}
-	for (uint8_t n = 0; n < value_len; n++) {
-		if (n == value[option][value_len - 1])
-			OLED_ShowChar(6 + n * 6, 30, '.', 12, (n == value_num && count) ? 0 : 1);
-		else if (n < value[option][value_len - 1])
-			OLED_ShowChar(6 + n * 6, 30, value[option][n] + '0', 12, (n == value_num && count) ? 0 : 1);
-		else
-			OLED_ShowChar(6 + n * 6, 30, value[option][n - 1] + '0', 12, (n == value_num && count) ? 0 : 1);
-	}
-	OLED_ShowString(6, 12, name[option], 12, 1);
-	OLED_Refresh();
-	OLED_ClearRF();
-	if (KEY_Scan(3)) {
-		car_screen_flag = 0;
-		sel_flag = 1;
-		OLED_ClearRF();
-
-		pidInit();
-
-		return;
-	}
-	if (KEY_Scan(4)) {
-		sel_flag = 1;
-		OLED_ClearRF();
-		return;
-	}
-	delay_ms(100);
-}
-void loop_screen0(void) {
-	if (KEY_Scan(3)) {
-		sel_flag = 0;
-		OLED_ClearRF();
-		return;
-	}
-	if (KEY_Scan(4)) {
-		car_screen_flag = 0;
-		OLED_ClearRF();
-
-		pidInit();
-
-		return;
-	}
-	if (KEY_Scan(1)) {
-		if (option < option_NUM - 1)
-			option++;
-		if (area < 2)
-			area++;
-	}
-	if (KEY_Scan(2)) {
-		if (option > 0)
-			option--;
-		if (area > 0)
-			area--;
-	}
-
-	OLED_ShowString(6, 12 + 18 * 0, name[option - area + 0], 12, 1);
-	OLED_ShowString(6, 12 + 18 * 1, name[option - area + 1], 12, 1);
-	OLED_ShowString(6, 12 + 18 * 2, name[option - area + 2], 12, 1);
-
-	OLED_DrawBoxLine(1, 9, 126, 25, 1);
-	OLED_DrawBoxLine(1, 27, 126, 43, 1);
-	OLED_DrawBoxLine(1, 45, 126, 61, 1);
-
-	if (count)
-		FuXuan(area);
-	else
-		FuXuan(area + 3);
-
-	OLED_Refresh();
-	OLED_ClearRF();
 }
 void pidInit(void) {
 	GA = value[0][0] * 10 + value[0][1];
